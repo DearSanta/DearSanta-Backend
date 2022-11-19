9853b5101eab6f5eaaaccf93c1f40e1e5bcd2bb3
seungmin Ryu <ojysep9987@naver.com>
2022-11-19 16:26:12 +0900

    HotFix: 로직 수정

--- letter/views.py
+++ letter/views.py
@@ -47,7 +47,7 @@ def write_answer(request):
         user = get_object_or_404(User, email=payload['email'])
 
 
-        answer = Answer.objects.filter(responser=user.pk).last()
+        answer = Answer.objects.filter(responser=user.pk).first()
         answer.content = data['content']
         answer.save()
         return HttpResponse(status=200)
